8a5c02ebe865068a2e973da58b196148f2b788de
Erika Hirata <rkaisomura@gmail.com>
2021-01-27 08:38:04 +0100

    Fixed the issue regarding post, get 404 (not found)

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,12 +1,30 @@
 import { text } from "body-parser";
 
+const postData = async (url, data = {}) => {
+    const resp = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        mode: 'cors',
+        headers: {
+            'Content-Type':'application/json',
+        },
+        body: JSON.stringify(data) //Stringify the data
+    });
+    try {
+        const newData = await resp.json(); //Jsonify the response
+        return newData;
+    } catch (error) {
+        console.log("Error", error);
+    }
+};
+
 async function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
     let urlUser = document.getElementById('homepage').value;
     if (Client.checkForUrl(urlUser)){
-        postData('http://localhost:8081/sentiment', {url: urlUser})
+        postData('http://localhost:8080/sentiment', {url: urlUser})
         .then(function(newData) {
             updateUI();
         });
@@ -15,30 +33,14 @@ async function handleSubmit(event) {
     }
 };
 
-const postData = async (url, data = {}) => {
-    const resp = await fetch(url, {
-        method: 'POST',
-        credentials: 'same-origin',
-        mode: 'cors',
-        headers: {
-            'Content-Type':'text/plaim',
-        },
-        body: text(data)
-    });
-    try {
-        const newData = await resp.text();
-        return newData;
-    } catch (error) {
-        console.log("Error", error);
-    }
-};
+
 
 //Update the UI with the results
 const updateUI = async() =>{
-    const url = "http://localhost:8081/sentiment";
+    const url = "http://localhost:8080/sentiment";
     const req = await fetch (url);
     try {
-        const info = await req.text();
+        const info = await req.json();
         document.getElementById('score').innerHTML = "Polarity: " + info.score;
         document.getElementById('agreement').innerHTML = "Agreement: " + info.agreement;
         document.getElementById('subjectivity').innerHTML = "Subjectivity: " + info.subjectivity;
--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,8 @@ dotenv.config();
 const keyApi = process.env.API_KEY;
 console.log(`API Key is ${keyApi}`);
 
+projectData = {};
+
 var path = require('path');
 const express = require('express');
 const app = express();
@@ -15,15 +17,15 @@ app.use(cors());
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false}));
-app.use(bodyParser.text());
+app.use(bodyParser.json());
 const mockAPIResponse = require('./mockAPI.js');
 
 //const apiURL = 'https://api.meaningcloud.com/sentiment-2.1?key=${keyApi}&text&model=general&lang=en&url=';
 const apiURL = 'https://api.meaningcloud.com/sentiment-2.1?';
-let urlInput = [];
+let urlInput = "";
 
 // designates what port the app will listen to for incoming requests
-const port = 8081;
+const port = 8080;
 app.listen(port, function () {
     console.log(`Listening on port ${port}!`);
 });
@@ -35,24 +37,28 @@ app.get('/', function (req, res) {
 //POST route
 app.post('/sentiment', insertPost);
 async function insertPost(req,resp){
+    console.log(urlInput);
     urlInput = req.body.url;
+    
     console.log(urlInput);
 
     const urlAPI = `${apiURL}key=${keyApi}&url=${urlInput}&lang=en`;
     const fetchResponse = await fetch(urlAPI);
     console.log(fetchResponse);
    
-    const sentimentData = await fetchResponse.text();
+    const sentimentData = await fetchResponse.json();
     console.log(sentimentData);
 
-    const projectData = {
-        score: sentimentData.score,
+    projectData = {
+        score: sentimentData.score_tag,
         agreement: sentimentData.agreement,
         subjectivity: sentimentData.subjectivity,
         irony: sentimentData.irony,
         confidence: sentimentData.confidence
     }
+    console.log("---------------->");
     console.log(projectData);
+    console.log("----------------<");
     resp.send(projectData);
 }
 
